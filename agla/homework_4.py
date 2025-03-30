c3991fc375da22df70c08efddefd3209b64900a4
Alina <alinapestova@gmail.com>
2025-03-30 13:40:17 +0300

    inno

--- agla/homework_4.py
+++ agla/homework_4.py
@@ -1,54 +1,32 @@
-def transpose(matrix):
-    return list(map(list, zip(*matrix)))
+import numpy as np
 
-def gaussian_elimination(matrix):
-    n = len(matrix)
-    m = len(matrix[0])
-    
-    for i in range(min(n, m)):
-        max_row = max(range(i, n), key=lambda r: abs(matrix[r][i]))
-        matrix[i], matrix[max_row] = matrix[max_row], matrix[i]
+def orthogonal_complement(matrix):
+    if not isinstance(matrix, np.ndarray) or matrix.ndim != 2:
+        return None
 
-        pivot = matrix[i][i]
-        if pivot != 0:
-            matrix[i] = [x / pivot for x in matrix[i]]
-        for j in range(i + 1, n):
-            factor = matrix[j][i]
-            matrix[j] = [x - factor * y for x, y in zip(matrix[j], matrix[i])]
-    
-    return matrix
+    # Transpose the matrix to make rows into columns
+    matrix_transpose = matrix.T
 
-def back_substitution(matrix):
-    n = len(matrix)
-    m = len(matrix[0])
-    solution = [0] * m
-    
-    for i in range(n - 1, -1, -1):
-        if matrix[i][i] != 0:
-            solution[i] = matrix[i][-1] - sum(matrix[i][j] * solution[j] for j in range(i + 1, m))
-    
-    return solution
+    # Use QR decomposition to get an orthonormal basis of the columns
+    q, r = np.linalg.qr(matrix_transpose)
 
-def find_orthogonal_complement(matrix):
-    n = len(matrix)
-    m = len(matrix[0])
+    # If the rank of the matrix is less than the number of columns, the orthogonal complement exists
+    if matrix.shape[1] > np.linalg.matrix_rank(matrix):
+        # The columns of Q that are orthogonal to the columns of the original matrix
+        # form the orthogonal complement
+        complement = q[:, np.linalg.matrix_rank(matrix):]
+        return complement
+    else:
+        return np.array([]) # Return an empty array if the orthogonal complement is zero
 
-    augmented_matrix = [row + [0] * n for row in matrix]
-    for i in range(n):
-        augmented_matrix[i][m + i] = 1
-    reduced_matrix = gaussian_elimination(augmented_matrix)
-    orthogonal_complement = []
-    for i in range(m, m + n):
-        orthogonal_complement.append([row[i] for row in reduced_matrix])
-    
-    return orthogonal_complement
+# Example 
+matrix1 = np.array([[1, 2, 3],
+                    [4, 5, 6]])
 
-matrix = [
-    [1, 2, 1],
-    [2, 4, 3],
-    [3, 6, 4]
-]
+complement1 = orthogonal_complement(matrix1)
 
-orthogonal_complement = find_orthogonal_complement(matrix)
-for i, vec in enumerate(orthogonal_complement):
-    print(f"Orthogonal complement vector {i+1}: {vec}")
+if complement1 is not None:
+    print("Orthogonal complement of matrix1:")
+    print(complement1)
+else:
+    print("Invalid input for matrix1")
